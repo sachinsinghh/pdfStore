6be36c0e7a5e4464116bc23be14fe197aa5bcb0d
Developer <sachinsingh5891@gmail.com>
2020-01-30 23:14:01 +0530

    header component done

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import search from './images/search.gif'; 
+import de from './images/de.gif'; 
+import en from './images/en.gif'; 
 
 
 const Header = () => {
     return (
-        <div className="top_search">
-            <p>schin singh</p>
+      <>
+       <div class="top_bar">
+
+      <div className="top_search">
+            <div className="search_text">
+              <a href="#">Advanced Search</a>
+              </div>
+            <input type="text" className="search_input" name="search" />
+            <input type="image" src={search} className="search_bt"/>
     </div>
+
+    
+    <div className="languages">
+      <div className="lang_text">Languages:</div>
+      <a href="#" className="lang"><img src={en} alt="" border="0" /></a>
+       <a href="#" className="lang">
+        <img src={de} alt="" border="0" />
+        </a> 
+      </div>
+      </div>
+      </>
     );
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Layout.css';
 
 
 const layout = (props) => (
     <>
+    <div className="main_container">
     {props.children}
+    </div>
     </>
 )
 
